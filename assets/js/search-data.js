69c6d9bb80fe314ed76f2abbaa92145b5f2e7e7e
ZhijinGuo <81092685+ZhijinGuo@users.noreply.github.com>
2026-06-14 08:58:59 +0000

    Deploying to gh-pages from @ ZhijinGuo/ZhijinGuo.github.io@b4c9fe9cece92c783d4090efcdca23fd7abd766f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,20 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-bookshelf",
-          title: "bookshelf",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/books/";
-          },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
